cd956f31da19b4f3f46e76af6bcffa06767e22c2
eejai42 <ej@ssot.me>
2024-01-23 00:47:25 -0600

    Star wars scroller added.

--- the-joneses/MartiniGlassScrollingSyntax/martini-scroller-app/src/TextScroller.js
+++ the-joneses/MartiniGlassScrollingSyntax/martini-scroller-app/src/TextScroller.js
@@ -3,42 +3,39 @@ import React, { useState, useEffect } from 'react';
 const TextScroller = ({ data, languageName, storyId }) => {
   const [currentVariationIndex, setCurrentVariationIndex] = useState(0);
   const [variations, setVariations] = useState([]);
-  const [keywords, setKeywords] = useState([]);
-
-  const scrollSpeed = 3000; // Adjust this value for different scroll speeds
-
+  const totalVariations = variations.length;
+  const scrollSpeed = 10000; // Duration of each scroll, e.g., 10 seconds
+  const overlapDuration = 5000; // Time after which next variation starts, e.g., 5 seconds
+  
   useEffect(() => {
-    // Find the matching story and language
     const story = data.story["syntax-locked-vs-unlocked"].find(s => s.id === storyId);
     if (story) {
       const languageVariations = story.languages[languageName];
       if (languageVariations) {
         setVariations(languageVariations.variations);
-        const languageKeywords = data.story.languages.find(lang => lang.name.toLowerCase() === languageName.toLowerCase());
-        if (languageKeywords) {
-          setKeywords(languageKeywords.keywords);
-        }
       }
     }
   }, [data, languageName, storyId]);
 
   useEffect(() => {
-    const interval = setInterval(() => {
-      setCurrentVariationIndex(currentIndex => 
-        (currentIndex + 1) % variations.length
-      );
-    }, scrollSpeed);
-
-    return () => clearInterval(interval);
-  }, [variations, scrollSpeed]);
+    if (totalVariations > 0) {
+      const interval = setInterval(() => {
+        setCurrentVariationIndex(currentIndex => 
+          (currentIndex + 1) % totalVariations
+        );
+      }, overlapDuration); // Start next variation after 2 seconds
+  
+      return () => clearInterval(interval);
+    }
+  }, [totalVariations, overlapDuration]);
 
   return (
-    <div className="TextScroller">
-            <div>
-                {languageName}: {storyId} - {keywords}
-            </div>
-
-      <p>{variations[currentVariationIndex]}</p>
+    <div className="FullHeightContainer">
+      {variations.map((variation, index) => (
+        <p key={index} className={`TextScroller ${index === currentVariationIndex ? 'active' : ''}`} style={{animationDuration: `${scrollSpeed}ms`}}>
+          {variation}
+        </p>
+      ))}
     </div>
   );
 };
