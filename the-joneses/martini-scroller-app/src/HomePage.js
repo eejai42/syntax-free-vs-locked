7654197af31d48bf7006f4cf04a5f1ccf69f263a
eejai42 <ej@ssot.me>
2024-02-02 10:58:12 -0600

    Cleaned up home page.

--- the-joneses/martini-scroller-app/src/HomePage.js
+++ the-joneses/martini-scroller-app/src/HomePage.js
@@ -2,13 +2,12 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const HomePage = () => {
-    console.error('Rending Home Page', HomePage);
   return (
     <div>
       <iframe
-        width="560"
-        height="315"
-        src="https://www.youtube.com/embed/[YourVideoID]"
+        width="1120"
+        height="630"
+        src="https://www.youtube.com/embed/jmDTNE4brLI?autoplay=1"
         title="YouTube video player"
         frameBorder="0"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
