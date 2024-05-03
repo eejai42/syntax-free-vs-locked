7762e3169d853d99006cb9887c3d5c5e801b95ed
eejai42 <ej@ssot.me>
2024-05-03 15:20:19 -0500

    bump.

--- the-joneses/martini-scroller-app/src/tech-should-flow/RowHeaderComponent.js
+++ the-joneses/martini-scroller-app/src/tech-should-flow/RowHeaderComponent.js
@@ -1,12 +1,23 @@
-import React, { useState } from 'react';
-import SaturationPicker from './SaturationPicker';
-import { getContrastColor, renderAttachments } from '../colorUtils';
+import React, { useState } from "react";
+import SaturationPicker from "./SaturationPicker";
+import { getContrastColor, renderAttachments } from "../colorUtils";
 
-const RowHeaderComponent = ({ transpilerNode, onTranspilerNodeChange, onUpdateClick }) => {
+const RowHeaderComponent = ({
+  transpilerNode,
+  onTranspilerNodeChange,
+  onUpdateClick,
+}) => {
   const baseImageUrl = "transpiler-images"; // Base URL for images, should be adjusted based on actual path
 
   // Helper to generate image paths
-  const imagePath = (attachmentName, index) => `${baseImageUrl}/${transpilerNode.FromNodeName.replace(/\s+/g, '_')}/${transpilerNode.EdgeName.replace(/\s+/g, '_').replace('#', '%23')}_${attachmentName}_${index}.png`;
+  const imagePath = (attachmentName, index) =>
+    `${baseImageUrl}/${transpilerNode.FromNodeName.replace(
+      /\s+/g,
+      "_"
+    )}/${transpilerNode.EdgeName.replace(/\s+/g, "_").replace(
+      "#",
+      "%23"
+    )}_${attachmentName}_${index}.png`;
 
   const handleColorCHange = (newColor) => {
     // Handle saturation change
@@ -14,8 +25,7 @@ const RowHeaderComponent = ({ transpilerNode, onTranspilerNodeChange, onUpdateCl
     if (onTranspilerNodeChange) {
       onTranspilerNodeChange(transpilerNode, newColor);
     }
-  }
-
+  };
 
   const handleUpdateClick = () => {
     if (onUpdateClick) {
@@ -24,76 +34,100 @@ const RowHeaderComponent = ({ transpilerNode, onTranspilerNodeChange, onUpdateCl
   };
 
   return (
-    <table style={{
-      width: '40em',
-      position: 'relative',
-      backgroundColor: transpilerNode.ExpectedColor,
-      color: getContrastColor(transpilerNode.ExpectedColor),
-
-    }}><tbody>
-    
+    <table
+      style={{
+        width: "40em",
+        position: "relative",
+        backgroundColor: transpilerNode.ExpectedColor,
+        color: getContrastColor(transpilerNode.ExpectedColor),
+      }}
+    >
+      <tbody>
         <tr>
-        <td>
-        {transpilerNode.FromNodeAttachments && transpilerNode.FromNodeAttachments.map((attachment, index) => (
-          <img
-            key={index}
-            src={imagePath('FromNodeAttachments', index)}
-            alt="From Node Attachment Image"
-            className="from-node-attachment"
-          />
-        ))}
-        <div style={{ color: 'white', fontWeight: 'bold', textAlign: 'center' }}>
-          {transpilerNode.FromNodeName} {transpilerNode.IsUsed === 0 && ' (unused)'} ➡
-        </div>
-
-        </td>
-        <td colSpan="2" style={{textAlign: 'center', width: '40%'}}>
-        {transpilerNode.TranspilerNodeAttachments && renderAttachments(transpilerNode.TranspilerNodeAttachments, transpilerNode.FromNodeName, transpilerNode.EdgeName, "TranspilerNodeAttachments")}
+          <td>
+            {transpilerNode.FromNodeAttachments &&
+              transpilerNode.FromNodeAttachments.map((attachment, index) => (
+                <div key={index}>
+                  <img
+                    src={imagePath("FromNodeAttachments", index)}
+                    alt="From Node Attachment Image"
+                    className="from-node-attachment"
+                  />
+                </div>
+              ))}
+            <div
+              style={{
+                color: "white",
+                fontWeight: "bold",
+                textAlign: "center",
+              }}
+            >
+              {transpilerNode.FromNodeName}{" "}
+              {transpilerNode.IsUsed === 0 && " (unused)"} ➡
+            </div>
+          </td>
+          <td colSpan="2" style={{ textAlign: "center", width: "40%" }}>
+            {transpilerNode.TranspilerNodeAttachments &&
+              renderAttachments(
+                transpilerNode.TranspilerNodeAttachments,
+                transpilerNode.FromNodeName,
+                transpilerNode.EdgeName,
+                "TranspilerNodeAttachments"
+              )}
             <h4>{transpilerNode.TransformerNodeName}</h4>
-        </td>
-        <td>
-        <div style={{backgroundColor: transpilerNode.ToNodeMasterColor}}>
-          {transpilerNode.ToNodePlatformAttachments && transpilerNode.ToNodePlatformAttachments.map((attachment, index) => (
-            <img
-              key={index}
-              src={imagePath('ToNodePlatformAttachments', index)}
-              alt="To Node Platform Attachment Image"
-              className="platform-image"
-            />
-          ))}
-        </div>
-
-        </td>
+          </td>
+          <td>
+            <div style={{ backgroundColor: transpilerNode.ToNodeMasterColor }}>
+              {transpilerNode.ToNodePlatformAttachments.map(
+                  (attachment, index) => (
+                    <div key={index}>
+                  {<SaturationPicker
+                    color={transpilerNode.ExpectedColor}
+                    isPickerAvailable={!transpilerNode.SyntaxFreeEdgeName}
+                    onColorChange={handleColorCHange}
+                    label={transpilerNode.EdgeName}
+                    src={imagePath("ToNodePlatformAttachments", index)}
+                    />}
+                    </div>
+                  )
+                )}
+            </div>
+          </td>
         </tr>
-        <tr><td colSpan="3" onClick={handleUpdateClick} style={{cursor: 'pointer', textAlign: 'center'}}> ⬅ Update Now ➡</td></tr>
-      <tr>
-      <td>
-
-
-</td>
-        <td colSpan="1">
-</td>
-<td></td>
-<td>
-              
-      <div style={{ color: 'white', fontWeight: 'bold', textAlign: 'center' }}>
-            {transpilerNode.EdgeName}
-          </div>
-
-        </td></tr>
+        <tr>
+          <td
+            colSpan="3"
+            onClick={handleUpdateClick}
+            style={{ cursor: "pointer", textAlign: "center" }}
+          >
+            {" "}
+            ⬅ Update Now ➡
+          </td>
+        </tr>
+        <tr>
+          <td></td>
+          <td colSpan="1"></td>
+          <td></td>
+          <td>
+            <div
+              style={{
+                color: "white",
+                fontWeight: "bold",
+                textAlign: "center",
+              }}
+            >
+              {transpilerNode.EdgeName}
+            </div>
+          </td>
+        </tr>
         {!transpilerNode.SyntaxFreeEdgeName && (
-        <tr><td colSpan="3">
-        <div>
-          <div>
-            {transpilerNode.ExpectedColor}
-          </div>
-            <SaturationPicker  color={transpilerNode.ExpectedColor} showPictures={true} onColorChange={handleColorCHange}  />
-          </div>
-
-          </td></tr>)};
-        </tbody>
+          <tr>
+            <td colSpan="3"></td>
+          </tr>
+        )}
+        ;
+      </tbody>
     </table>
-
   );
 };
 
--- the-joneses/martini-scroller-app/src/tech-should-flow/SaturationPicker.js
+++ the-joneses/martini-scroller-app/src/tech-should-flow/SaturationPicker.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { HexColorPicker } from "react-colorful";
 import { hslToHex, hexToHsl } from "../colorUtils";
 
-const SaturationPicker = ({ color, onColorChange, showPictures}) => {
+const SaturationPicker = ({ color, onColorChange, src, label = "", isPickerAvailable=false}) => {
 
     let transpilerNode = null;
     const [showPicker, setShowPicker] = useState(false);
@@ -15,21 +15,21 @@ const SaturationPicker = ({ color, onColorChange, showPictures}) => {
     };
 
     return (
-        <div style={{ width: "10em", height: "11em", margin: "0.5em", border: 'solid 3px black', 
-                      backgroundColor: currentColor, borderRadius: '0.45em', cursor: 'pointer' }} 
+        <div style={{ width: "100%", height: "15em", margin: "0.5em", marginRight: '1em;', border: (isPickerAvailable ? 'solid 3px black' : ''), 
+                       borderRadius: '0.45em', cursor: (isPickerAvailable ? 'pointer' : '') }} 
         className="outerHoverDiv"
-        onClick={() => setShowPicker(true)}  // Set to true on click
+        onClick={() => isPickerAvailable ? setShowPicker(true) : null}  // Set to true on click
         onMouseLeave={() => setShowPicker(false)}  
         >
             <div style={{ width: "10em", height: "10em" }}>
-                <div className="pickerLabel">{currentColor}</div>
+                <div className="pickerLabel">{label || currentColor}</div>
                 <div style={{ position: "relative", width: "10em", height: "10em", margin: "0.8em", overflow: "hidden" }}>
-                    <div className="presetBtnContainer" style={{ position: "absolute", top: 0, left: '-4em', width: "14.5em", height: "6em", overflow: "hidden", display: 'block'}}>
+                    <div className="presetBtnContainer" style={{ position: "absolute", top: 0, left: '0em', width: "14.5em", height: "8em", overflow: "hidden", display: 'block'}}>
                         <div style={{display: showPicker ? 'block' : 'none' }}>
                         <HexColorPicker color={currentColor} onChange={handleColorChange}  />
                         </div>
-                        <div style={{display: !showPicker && showPictures ? 'block' : 'none' }}>
-                            <img src={transpilerNode?.LockedImage?.length ? transpilerNode.LockedImage : transpilerNode?.SyntaxFreeImage} style={{width: '8.5em', paddingLeft: '0.5em', marginLeft: '0.5em'}}/>
+                        <div style={{display: !showPicker && src ? 'block' : 'none' }}>
+                            <img src={src} style={{width: '8.5em', paddingLeft: '0.5em', marginLeft: '0.5em'}}/>
                         </div>
                     </div>
                 </div>
