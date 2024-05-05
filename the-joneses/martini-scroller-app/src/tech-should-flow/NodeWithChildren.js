047323048ed4aca0cb4fa52bac3d2bf5e8274d0b
eejai42 <ej@ssot.me>
2024-05-05 18:10:39 -0500

    bump

--- the-joneses/martini-scroller-app/src/tech-should-flow/NodeWithChildren.js
+++ the-joneses/martini-scroller-app/src/tech-should-flow/NodeWithChildren.js
@@ -6,24 +6,46 @@ const NodeWithChildren = ({ node }) => {
             <tbody>
                 {/* Display the current node's main details */}
                 <tr>
-                    <td colSpan={node.Children ? node.Children.length : 1}>
-                        <div>
-                            <strong>Name:</strong> {node.Name}<br/>
-                            <strong>FQNChoiceName:</strong> {node.FQNChoiceName}<br/>
-                            <strong>ToolName:</strong> {node.ToolName}<br/>
-                            <strong>NodeName:</strong> {node.NodeName}<br/>
-                            <strong>ExpectedColor:</strong> {node.ExpectedColor}<br/>
-                            <strong>Notes:</strong> {node.Notes || "No notes provided."}
-                        </div>
+                    <td colSpan={node.Children ? node.Children.length * 2 : 1} style={{border: 'none'}}>
+                        <table class="node-header">
+                            <tr>
+                                <td>
+                                <div style={{border: node.MOFLayerNumber < 2 ? 'solid black 1px' : 'none'}}>
+                                    <strong>{node.FQNChoiceName}</strong><br/>
+                                    <div>
+                                        <strong>ExpectedColor:</strong> {node.ExpectedColor}<br/>
+                                    </div>
+                                    <div>
+                                        <strong>MOFLayer:</strong> {node.MOFLayerNumber}<br/>
+                                    </div>
+                                </div>
+
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <div>
+                                        {node.ToolName ? (<span><strong></strong> {node.ToolName} </span>) : null}
+                                        {node.InputChoiceFileName ? <span> -i<strong> {node.InputChoiceFileName}</strong></span> : null}
+                                        {node.NodeDefaultFileName ? <span> -o<strong> {node.NodeDefaultFileName}</strong></span> : null}
+                                    </div>
+                                </td>
+                            </tr>
+                        </table>                                    
                     </td>
                 </tr>
+                {/* <tr><td></td></tr> */}
                 {/* Display children in separate cells if they exist */}
                 {node.Children && node.Children.length > 0 && (
                     <tr>
-                        {node.Children.map(childNode => (
-                            <td key={childNode.NodeName}>
-                                <NodeWithChildren node={childNode} />
-                            </td>
+                        {node.Children.map((childNode, index) => (                            
+                            <React.Fragment>
+                                {(childNode.MOFLayerNumber !== 1) || index === 0 ?
+                                <td key={childNode.NodeName}>   
+                                    Parentalyer: {node.ParentMOFLayerNumber || '--'} / {childNode.ParentMOFLayerNumber || '--'} / LayerNumber: {childNode.MOFLayerNumber || '--'}
+                                    <NodeWithChildren node={childNode} />
+                                </td> : <td key={childNode.NodeName} style={{border: 'none'}}></td>}
+                            </React.Fragment>
                         ))}
                     </tr>
                 )}
