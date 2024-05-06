35081533d61d3e9dc000d1cb81bb58c07f4322c0
eejai42 <ej@ssot.me>
2024-05-06 17:53:17 -0500

    bump.

--- the-joneses/martini-scroller-app/src/tech-should-flow/NodeWithChildren.js
+++ the-joneses/martini-scroller-app/src/tech-should-flow/NodeWithChildren.js
@@ -7,8 +7,13 @@ const NodeWithChildren = ({ node, selectedIndex }) => {
     node.MOFLayerNumber != 13 ? node.NodeDesiredColor || "#000000" : "#FFFFFF"
   );
   const [mixedColor, setMixedColor] = useState(node.MixedColor || "#000000");
-  const [parentColor, setParentColor] = useState(node.ParentNodeDesiredColor || "#000000");
-  const [desiredColor, setDesiredColor] = useState(node.DesiredColor || "#000000");
+  const [parentColor, setParentColor] = useState(
+    node.ParentNodeDesiredColor || "#000000"
+  );
+  const [desiredColor, setDesiredColor] = useState(
+    node.DesiredColor || "#000000"
+  );
+  const [isPicking, setIsPicking] = useState(false);
 
   useEffect(() => {
     setNodeDesiredColor(node.NodeDesiredColor || "#000000");
@@ -17,20 +22,23 @@ const NodeWithChildren = ({ node, selectedIndex }) => {
   const handleNodeDesiredColorChange = (color) => {
     setNodeDesiredColor(color);
     if (node.MOFLayerNumber === 0) {
-        setMixedColor(mixColors(color, desiredColor));
+      setMixedColor(mixColors(color, desiredColor));
     } else {
-        setMixedColor(color);    
+      setMixedColor(color);
     }
   };
 
-  const handleDesiredColorChange = (color) => { 
+  const handleDesiredColorChange = (color) => {
     setDesiredColor(color);
     if (node.MOFLayerNumber === 0) {
-        setMixedColor(mixColors(color, nodeDesiredColor));
+      setMixedColor(mixColors(color, nodeDesiredColor));
     } else {
-        setMixedColor(color);    
+      setMixedColor(color);
     }
-};
+  };
+  const handleIsPickingChange = (isPicking) => {
+    setIsPicking(isPicking);
+  };
 
   const pushExpectedColorForward = () => {
     console.error(
@@ -54,20 +62,30 @@ const NodeWithChildren = ({ node, selectedIndex }) => {
           >
             <table class="node-header">
               <tr>
-
                 {/* Inner (left orient) RIGHT side of screen */}
-                
+
                 {node.MOFLayerNumber <= 0 && node.OutputIsDocs && (
-                  <td style={{ width: "20em", position: "relative"}}>
-                    <div style={{ backgroundColor: desiredColor, color: getContrastColor(desiredColor), borderRadius: '2em', margin: '0.5em', marginTop: '0em' }}>
-                    {node.ToolTransformerFileName || node.ToolName || node.NodeDefaultFileName}
-                    <SaturationPicker
-                      color={desiredColor}
-                      src={node?.ToolAttachments[0].url}
-                      onColorChange={handleDesiredColorChange}
-                      isPickerAvailable={true}
-                      style={{ height: "" }}
-                    ></SaturationPicker>
+                  <td style={{ width: "20em", position: "relative" }}>
+                    <div
+                      style={{
+                        backgroundColor: desiredColor,
+                        color: getContrastColor(desiredColor),
+                        borderRadius: "2em",
+                        margin: "0.5em",
+                        marginTop: "0em",
+                      }}
+                    >
+                      {node.ToolTransformerFileName ||
+                        node.ToolName ||
+                        node.NodeDefaultFileName}
+                      <SaturationPicker
+                        color={desiredColor}
+                        src={node?.ToolAttachments[0].url}
+                        onColorChange={handleDesiredColorChange}
+                        isPickerAvailable={true}
+                        onIsPickingChange={handleIsPickingChange}
+                        style={{ height: "" }}
+                      ></SaturationPicker>
                     </div>
                   </td>
                 )}
@@ -75,7 +93,7 @@ const NodeWithChildren = ({ node, selectedIndex }) => {
                   <div
                     style={{
                       position: "relative",
-                      height: node.MOFLayerNumber === 1 ? "14em" : "14em",
+                      height: node.MOFLayerNumber >= 1 ? "12em" : "15em",
                       border:
                         node.MOFLayerNumber < 2 ? "solid black 1px" : "none",
                       backgroundColor: nodeDesiredColor,
@@ -85,37 +103,37 @@ const NodeWithChildren = ({ node, selectedIndex }) => {
                       marginRight: "auto",
                     }}
                   >
-                    {/* LEFT  Node - CODE*/}
-                    {(node.MOFLayerNumber === 0) && !node.OutputIsDocs && (
-                        <div
-                          style={{
-                            width: "20em",
-                            position: "absolute",
-                            top: "1em",
-                            left: "2.5em",
-                          }}
-                        >
-                          <SaturationPicker
-                            color={nodeDesiredColor}
-                            src={node?.NodeAttachments[0].url}
-                            onColorChange={handleNodeDesiredColorChange}
-                            label={
-                              node.MOFLayerNumber != 1
-                                ? "change"
-                                : node.ParentChoiceName
-                            }
-                            isPickerAvailable={node.MOFLayerNumber != 3}
-                            style={{ height: "" }}
-                          ></SaturationPicker>
-                        </div>
-                      )}
+                    {/* LEFT  Node - Source Code */}
 
-                    <h3>
-                      {node.FQNChoiceName} 
-                    </h3>
+                    {node.MOFLayerNumber === 0 && !node.OutputIsDocs && (
+                      <div
+                        style={{
+                          width: "20em",
+                          position: "absolute",
+                          top: "0em",
+                          left: "2.5em",
+                        }}
+                      >
+                        <SaturationPicker
+                          color={nodeDesiredColor}
+                          src={node?.NodeAttachments[0].url}
+                          onColorChange={handleNodeDesiredColorChange}
+                          onIsPickingChange={handleIsPickingChange}
+                          label={
+                            node.MOFLayerNumber != 1
+                              ? "change"
+                              : node.ParentChoiceName
+                          }
+                          isPickerAvailable={node.MOFLayerNumber != 3}
+                          style={{ height: "" }}
+                        ></SaturationPicker>
+                      </div>
+                    )}
+
+                    <h3>{node.FQNChoiceName}</h3>
                     {/* MIDDLE - Center Aligned Picker*/}
 
-                    {(node.MOFLayerNumber >= 1) && (
+                    {node.MOFLayerNumber >= 1 && (
                       <div
                         style={{
                           width: "55%",
@@ -128,6 +146,7 @@ const NodeWithChildren = ({ node, selectedIndex }) => {
                           color={nodeDesiredColor}
                           src={node?.NodeAttachments[0].url}
                           onColorChange={handleNodeDesiredColorChange}
+                          onIsPickingChange={handleIsPickingChange}
                           label={
                             node.MOFLayerNumber != 1
                               ? "change"
@@ -142,84 +161,101 @@ const NodeWithChildren = ({ node, selectedIndex }) => {
                         ></SaturationPicker>
                         <div></div>
                       </div>
-                    )}                    
+                    )}
 
-                    {/* RIGHT  Node*/}
-                    {(node.MOFLayerNumber === 0) && node.OutputIsDocs && (
-                        <div
-                          style={{
-                            width: "20em",
-                            position: "absolute",
-                            top: "1em",
-                            right: "2.5em",
-                          }}
-                        >
-                          <SaturationPicker
-                            color={nodeDesiredColor}
-                            src={node?.NodeAttachments[0].url}
-                            onColorChange={handleNodeDesiredColorChange}
-                            label={
-                              node.MOFLayerNumber != 1
-                                ? "change"
-                                : node.FQNChoiceName
-                            }
-                            isPickerAvailable={node.MOFLayerNumber != 3}
-                            style={{ height: "" }}
-                          ></SaturationPicker>{" "}
-                        </div>
-                      )}
+                    {/* RIGHT  Node - Documentation*/}
+                    {node.MOFLayerNumber === 0 && node.OutputIsDocs && (
+                      <div
+                        style={{
+                          width: "20em",
+                          position: "absolute",
+                          top: "0em",
+                          right: "2.5em",
+                        }}
+                      >
+                        <SaturationPicker
+                          color={nodeDesiredColor}
+                          src={node?.NodeAttachments[0].url}
+                          onColorChange={handleNodeDesiredColorChange}
+                          onIsPickingChange={handleIsPickingChange}
+                          label={
+                            node.MOFLayerNumber != 1
+                              ? "change"
+                              : node.FQNChoiceName
+                          }
+                          isPickerAvailable={node.MOFLayerNumber != 3}
+                          style={{ height: "" }}
+                        ></SaturationPicker>{" "}
+                      </div>
+                    )}
                   </div>
                 </td>
-                
-                {/* inner RIGHT oriented, LEFT side of screen*/}
-                
-                {node.MOFLayerNumber <= 0 && !node.OutputIsDocs && (
-                  <td style={{ width: "20em", position: "relative"}}>
-                    <div style={{ backgroundColor: desiredColor, borderRadius: '2em', margin: '0.5em', marginTop: '0em' }}>
 
-                        <SaturationPicker
+                {/* inner RIGHT oriented, LEFT side of screen*/}
+                {node.MOFLayerNumber <= 0 && !node.OutputIsDocs && (
+                  <td style={{ width: "20em", position: "relative" }}>
+                    <div
+                      style={{
+                        backgroundColor: desiredColor,
+                        borderRadius: "2em",
+                        margin: "0.5em",
+                        marginTop: "0em",
+                      }}
+                    >
+                      {node.ToolTransformerFileName ||
+                        node.ToolName ||
+                        node.NodeDefaultFileName}
+
+                      <SaturationPicker
                         label={node.InputChoiceFileName}
                         color={desiredColor}
                         src={node?.ToolAttachments[0].url}
                         onColorChange={handleDesiredColorChange}
+                        onIsPickingChange={handleIsPickingChange}
                         isPickerAvailable={true}
-                        ></SaturationPicker>                
+                      ></SaturationPicker>
                     </div>
                   </td>
                 )}
               </tr>
-              <tr>
-                <td colSpan="2">
-                  <div
-                    class="output-node"
-                    style={{
-                      marginRight: "auto",
-                      marginLeft: "auto",
-                      maxWidth: "30em",
-                      backgroundColor: mixedColor,
-                      color: getContrastColor(mixedColor),
-                    }}
-                  >
-                    {node.InputChoiceFileName ? (
-                      <span>
-                        {" "}
-                        with <strong> {node.InputChoiceFileName}</strong>
-                      </span>
-                    ) : null}
-                    {node.ToolName ? (
-                      <span>
-                        <strong></strong> {node.ToolName}{" "}
-                      </span>
-                    ) : null}
-                    {node.NodeDefaultFileName ? (
-                      <span>
-                        {" "}
-                        creates <strong> {node.NodeDefaultFileName}</strong>
-                      </span>
-                    ) : null}
-                  </div>
-                </td>
-              </tr>
+                <tr>
+                  <td colSpan="2">
+                    <div
+                      class="output-node"
+                      style={{
+                        marginRight: "auto",
+                        marginLeft: "auto",
+                        maxWidth: "30em",
+                        minHeight: "3em",
+                        backgroundColor: mixedColor,
+                        color: getContrastColor(mixedColor),
+                      }}
+                    >
+                    {!isPicking && (<div>
+
+                      {node.InputChoiceFileName ? (
+                        <span>
+                          {" "}
+                          with <strong> {node.InputChoiceFileName}</strong>
+                        </span>
+                      ) : null}
+                      {node.ToolName ? (
+                        <span>
+                          <strong></strong> {node.ToolName}{" "}
+                        </span>
+                      ) : null}
+                      {node.NodeDefaultFileName ? (
+                        <span>
+                          {" "}
+                          creates the{" "}
+                          <strong> {node.NodeDefaultFileName}</strong>
+                        </span>
+                      ) : null}
+                    </div>)}
+
+                    </div>
+                  </td>
+                </tr>
             </table>
           </td>
         </tr>
--- the-joneses/martini-scroller-app/src/tech-should-flow/SaturationPicker.js
+++ the-joneses/martini-scroller-app/src/tech-should-flow/SaturationPicker.js
@@ -3,7 +3,7 @@ import { HexColorPicker } from "react-colorful";
 import { hslToHex, hexToHsl } from "../colorUtils";
 import { getContrastColor } from "../colorUtils";
 
-const SaturationPicker = ({ color, onColorChange, src, label = "", isPickerAvailable=false, left='0em'}) => {
+const SaturationPicker = ({ color, onColorChange, src, label = "", isPickerAvailable=false, left='0em', onIsPickingChange}) => {
 
     let transpilerNode = null;
     const [showPicker, setShowPicker] = useState(false);
@@ -15,17 +15,22 @@ const SaturationPicker = ({ color, onColorChange, src, label = "", isPickerAvail
         onColorChange(newColor);  // Ensure external changes are also pushed up
     };
 
+    const handleSetShowPicker = (show) => { 
+        setShowPicker(show);
+        onIsPickingChange(show);
+    };
+
     return (
-        <div style={{ width: "90%", height: "14em", margin: "0.5em", marginRight: '1em;', border: (false && isPickerAvailable? 'solid 1px #efefef' : ''), // commenging out temporarily
+        <div style={{ width: "90%", height: "14em", marginRight: '1em;', border: (false && isPickerAvailable? 'solid 1px #efefef' : ''), // commenging out temporarily
                        borderRadius: '0.45em', cursor: (isPickerAvailable ? 'pointer' : '') }} 
         className="outerHoverDiv"
-        onClick={() => isPickerAvailable ? setShowPicker(true) : null}  // Set to true on click
-        onMouseLeave={() => setShowPicker(false)}  
+        onClick={() => isPickerAvailable ? handleSetShowPicker(true) : null}  // Set to true on click
+        onMouseLeave={() => handleSetShowPicker(false)}  
         >
             <div style={{ width: "18em", height: "15em", backgroundColor: {color} }}>
                 {false && isPickerAvailable && <div className="pickerLabel" style={{backgroundColor: currentColor, color: getContrastColor(currentColor)}}>{label || currentColor}</div>}
-                <div style={{ position: "absolute", width: "18em", paddingLeft: '1em', height: "10em", top: '1em', margin: "0.8em" }}>
-                    <div className="presetBtnContainer" style={{ position: "relative", top: '-1x', left: left, width: "14.5em", height: "16em", overflow: "hidden", display: 'block', zIndex: 9999999}}>
+                <div style={{ position: "absolute", width: "18em", paddingLeft: '1em', height: "10em", top: '1em' }}>
+                    <div className="presetBtnContainer" style={{ position: "relative", top: '-1x', left: left, width: "14.5em", height: "12em", overflow: "hidden", display: 'block', zIndex: 9999999}}>
                         <div style={{display: showPicker ? 'block' : 'none', position: 'relative' }}>
                         <HexColorPicker style={{border: '', height: ''}} color={currentColor} onChange={handleColorChange}  />
                         </div>
