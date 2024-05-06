554cd8d717c2bb7e44b0677956be2bb254798666
eejai42 <ej@ssot.me>
2024-05-05 23:43:11 -0500

    bump.

--- the-joneses/martini-scroller-app/src/tech-should-flow/NodeWithChildren.js
+++ the-joneses/martini-scroller-app/src/tech-should-flow/NodeWithChildren.js
@@ -10,7 +10,8 @@ const NodeWithChildren = ({ node, selectedIndex }) => {
         setCurrentColor(node.NodeDesiredColor || "#000000");
     }, [node.NodeDesiredColor]);
 
-    const handleHueChange = (hue) => {
+    const handleHueChange = (color) => {
+        setCurrentColor(color);
     };
     
     return (
@@ -21,11 +22,16 @@ const NodeWithChildren = ({ node, selectedIndex }) => {
                     <td colSpan={node.Children ? node.Children.length * 2 : 1} style={{border: 'none'}}>
                         <table class="node-header">
                             <tr>
-                                {node.MOFLayerNumber === 0 && !node.OutputIsDocs && <td style={{width: '10em', position: 'relative'}}>
-                                    <SaturationPicker color={currentColor} onColorChange={setCurrentColor} isPickerAvailable={true} style={{ height: ''}}></SaturationPicker>
+                                {node.MOFLayerNumber <= 0 && !node.OutputIsDocs && <td style={{width: '20em', position: 'relative'}}>
+                                    <SaturationPicker label={node.InputChoiceFileName} color={currentColor} src={node?.NodeAttachments[0].url} onColorChange={handleHueChange} isPickerAvailable={true} style={{ height: ''}}></SaturationPicker>
                                 </td>}
                                 <td>
-                                    <div style={{border: node.MOFLayerNumber < 2 ? 'solid black 1px' : 'none', backgroundColor: node.NodeDesiredColor, color: getContrastColor(node.NodeDesiredColor)}}>
+                                    <div style={{position: 'relative', height: node.MOFLayerNumber === 3 ? '3em' : '18em', border: node.MOFLayerNumber < 2 ? 'solid black 1px' : 'none', backgroundColor: currentColor, color: getContrastColor(currentColor)}}>
+
+                                    {node.MOFLayerNumber <= 1 && !node.OutputIsDocs && <div style={{width: '20em', position: 'absolute', left: '3em'}}>
+                                        <SaturationPicker color={currentColor} src={node?.NodeAttachments[0].url} onColorChange={handleHueChange} isPickerAvailable={true} style={{ height: ''}}></SaturationPicker>
+                                    </div>}
+
                                         <strong>{node.FQNChoiceName}</strong>
                                         {node.MOFLayerNumber === 2 && <span>
                                             
@@ -33,10 +39,15 @@ const NodeWithChildren = ({ node, selectedIndex }) => {
                                             <div>{node.ExpectedColor && <span><strong>ExpectedColor:</strong> {node.ExpectedColor}<br/></span>}</div>
                                             <div>{node.NodeDesiredColor && <span><strong>NodeDesiredColor:</strong> {node.NodeDesiredColor}<br/></span>}</div>
                                             <div>{node.MixedColor && <span><strong>MixedColor:</strong> {node.MixedColor}<br/></span>}</div>
+
+                                    {node.MOFLayerNumber <= 1 && node.OutputIsDocs && <div style={{width: '20em', position: 'absolute', right: '3em'}}>
+                                        <SaturationPicker color={currentColor} src={node?.NodeAttachments[0].url} onColorChange={handleHueChange} isPickerAvailable={true} style={{ height: ''}}></SaturationPicker>
+                                    </div>}
+
                                     </div>
                                 </td>
-                                {node.MOFLayerNumber === 0 && node.OutputIsDocs && <td style={{width: '10em', position: 'relative'}}>
-                                    <SaturationPicker color={currentColor} onColorChange={setCurrentColor} isPickerAvailable={true}></SaturationPicker>
+                                {node.MOFLayerNumber <= 0 && node.OutputIsDocs && <td style={{width: '20em', position: 'relative'}}>
+                                    <SaturationPicker label={node.InputChoiceFileName} color={currentColor} src={node?.NodeAttachments[0].url} onColorChange={handleHueChange} isPickerAvailable={true}></SaturationPicker>
                                 </td>}
                             </tr>
                             <tr>
--- the-joneses/martini-scroller-app/src/tech-should-flow/SaturationPicker.js
+++ the-joneses/martini-scroller-app/src/tech-should-flow/SaturationPicker.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { HexColorPicker } from "react-colorful";
 import { hslToHex, hexToHsl } from "../colorUtils";
+import { getContrastColor } from "../colorUtils";
 
 const SaturationPicker = ({ color, onColorChange, src, label = "", isPickerAvailable=false}) => {
 
@@ -15,21 +16,21 @@ const SaturationPicker = ({ color, onColorChange, src, label = "", isPickerAvail
     };
 
     return (
-        <div style={{ width: "100%", height: "15em", margin: "0.5em", marginRight: '1em;', border: (isPickerAvailable ? 'solid 3px black' : ''), 
+        <div style={{ width: "90%", height: "18em", margin: "0.5em", marginRight: '1em;', border: (isPickerAvailable ? 'solid 1px lightgray' : ''), 
                        borderRadius: '0.45em', cursor: (isPickerAvailable ? 'pointer' : '') }} 
         className="outerHoverDiv"
         onClick={() => isPickerAvailable ? setShowPicker(true) : null}  // Set to true on click
         onMouseLeave={() => setShowPicker(false)}  
         >
-            <div style={{ width: "10em", height: "10em", backgroundColor: {color} }}>
-                <div className="pickerLabel" style={{backgroundColor: currentColor}}>{label || currentColor}</div>
-                <div style={{ position: "absolute", width: "10em", height: "10em", top: '2em', margin: "0.8em", overflow: "hidden" }}>
-                    <div className="presetBtnContainer" style={{ position: "relative", top: '0px', left: '-7em', width: "14.5em", height: "19em", overflow: "hidden", display: 'block', zIndex: 9999999}}>
+            <div style={{ width: "18em", height: "15em", backgroundColor: {color} }}>
+                <div className="pickerLabel" style={{backgroundColor: currentColor, color: getContrastColor(currentColor)}}>{label || currentColor}</div>
+                <div style={{ position: "absolute", width: "18em", height: "10em", top: '2em', margin: "0.8em" }}>
+                    <div className="presetBtnContainer" style={{ position: "relative", top: '0px', left: '-1em', width: "14.5em", height: "19em", overflow: "hidden", display: 'block', zIndex: 9999999}}>
                         <div style={{display: showPicker ? 'block' : 'none', position: 'relative' }}>
                         <HexColorPicker style={{border: '', height: ''}} color={currentColor} onChange={handleColorChange}  />
                         </div>
                         <div style={{display: !showPicker && src ? 'block' : 'none' }}>
-                            <img src={src} style={{width: '8.5em', paddingLeft: '0.5em', marginLeft: '0.5em'}}/>
+                            <img src={src} style={{width: '9.5em', paddingLeft: '0em', marginLeft: '0em'}}/>
                         </div>
                     </div>
                 </div>
