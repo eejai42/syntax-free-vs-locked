49ada564c398c56e6af7a4cd604420ed1b1c6af6
eejai42 <ej@ssot.me>
2024-08-01 12:54:36 -0500

    Cleaned up python the tree.

---
+++ python/run-trial.ps1
@@ -0,0 +1,24 @@
+python ./add-data.py add-root 20 1020 
+python ./add-data.py add-root 20 1026 
+
+python ./add-data.py add-generation 20 1021 1020
+python ./add-data.py add-generation 20 1030 1026
+
+python ./add-data.py add-generation 20 1022 1021
+python ./add-data.py add-generation 20 1031 1030
+
+python ./add-data.py add-generation 20 1027 1022
+python ./add-data.py add-generation 20 1032 1031
+
+python ./add-data.py add-generation 20 1033 1032
+python ./add-data.py add-generation 20 1038 1028
+
+python ./add-data.py add-generation 20 1039 1038
+python ./add-data.py add-generation 20 1035 1033
+
+
+
+
+
+
+
