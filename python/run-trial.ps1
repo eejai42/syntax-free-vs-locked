4bddd0a2fdc2012df663f5a3fea99b150066d24f
eejai42 <ej@ssot.me>
2024-08-03 18:39:27 -0500

    about to generate much more data.

--- python/run-trial.ps1
+++ python/run-trial.ps1
@@ -1,20 +1,20 @@
-python ./add-data.py add-root 20 1020 
-python ./add-data.py add-root 20 1026 
+python ./add-data.py add-root 1 1020 
+python ./add-data.py add-root 1 1026 
 
-python ./add-data.py add-generation 20 1021 1020
-python ./add-data.py add-generation 20 1030 1026
+python ./add-data.py add-generation 1 1021 1020
+python ./add-data.py add-generation 1 1030 1026
 
-python ./add-data.py add-generation 20 1022 1021
-python ./add-data.py add-generation 20 1031 1030
+python ./add-data.py add-generation 1 1022 1021
+python ./add-data.py add-generation 1 1031 1030
 
-python ./add-data.py add-generation 20 1027 1022
-python ./add-data.py add-generation 20 1032 1031
+python ./add-data.py add-generation 1 1027 1022
+python ./add-data.py add-generation 1 1032 1031
 
-python ./add-data.py add-generation 20 1033 1032
-python ./add-data.py add-generation 20 1038 1028
+python ./add-data.py add-generation 1 1033 1032
+python ./add-data.py add-generation 1 1038 1027
 
-python ./add-data.py add-generation 20 1039 1038
-python ./add-data.py add-generation 20 1035 1033
+python ./add-data.py add-generation 1 1039 1038
+python ./add-data.py add-generation 1 1035 1033
 
 
 
